adb5a07629ba481df389cf12c29164256707f850
Joe Wilm <joe@gameclosure.com>
2015-02-18 14:53:06 -0800

    Fix InputPrompt for iOS8

--- tealeaf/js/jsInput.mm
+++ tealeaf/js/jsInput.mm
@@ -17,51 +17,51 @@ static js_core *m_js;
 
 JSAG_MEMBER_BEGIN(openPrompt, 8)
 {
-	JSAG_ARG_NSTR(title);
-	JSAG_ARG_NSTR(msg);
-    JSAG_ARG_NSTR(okText);
-    JSAG_ARG_NSTR(cancelText);
-	JSAG_ARG_NSTR(value);
-	JSAG_ARG_BOOL(autoShowKeyboard);
-	JSAG_ARG_BOOL(isPassword);
-	JSAG_ARG_INT32(keyboardType);
-    
-	int32_t id = [[InputPrompt get] showAlertViewWithTitle:title
-                                          message:msg
-                                           okText:okText
-                                       cancelText:cancelText
-                                            value:value
-                                 autoShowKeyboard:autoShowKeyboard
-                                       isPassword:isPassword
-                                     keyboardType:keyboardType
-                                     returnButton:@""];
-    
-	JSAG_RETURN_INT32(id);
+  JSAG_ARG_NSTR(title);
+  JSAG_ARG_NSTR(msg);
+  JSAG_ARG_NSTR(okText);
+  JSAG_ARG_NSTR(cancelText);
+  JSAG_ARG_NSTR(value);
+  JSAG_ARG_BOOL(autoShowKeyboard);
+  JSAG_ARG_BOOL(isPassword);
+  JSAG_ARG_INT32(keyboardType);
+
+  int32_t id = [[InputPrompt get] showAlertViewWithTitle:title
+                                                 message:msg
+                                                  okText:okText
+                                              cancelText:cancelText
+                                                   value:value
+                                        autoShowKeyboard:autoShowKeyboard
+                                              isPassword:isPassword
+                                            keyboardType:keyboardType
+                                            returnButton:@""];
+
+  JSAG_RETURN_INT32(id);
 }
 JSAG_MEMBER_END
 
 JSAG_MEMBER_BEGIN(showKeyboard, 6)
 {
-	JSAG_ARG_NSTR(currVal);
-	JSAG_ARG_NSTR(hint);
-	JSAG_ARG_BOOL(hasBackward);
-	JSAG_ARG_BOOL(hasForward);
-	JSAG_ARG_NSTR(inputType);
-    JSAG_ARG_NSTR(inputReturnButton);
-	JSAG_ARG_INT32(maxLength);
-    JSAG_ARG_INT32(cursorPos);
-    
-	InputAccessory *ip = [InputAccessory get];
-	ip.hint = hint;
-	ip.currVal = currVal;
-	ip.inputType = inputType;
-    ip.inputReturnButton = inputReturnButton;
-	ip.hasForward = hasForward;
-	ip.hasBackward = hasBackward;
-	ip.maxLength = maxLength;
-    ip.cursorPos = cursorPos;
-    
-    [ip show];
+  JSAG_ARG_NSTR(currVal);
+  JSAG_ARG_NSTR(hint);
+  JSAG_ARG_BOOL(hasBackward);
+  JSAG_ARG_BOOL(hasForward);
+  JSAG_ARG_NSTR(inputType);
+  JSAG_ARG_NSTR(inputReturnButton);
+  JSAG_ARG_INT32(maxLength);
+  JSAG_ARG_INT32(cursorPos);
+
+  InputAccessory *ip = [InputAccessory get];
+  ip.hint = hint;
+  ip.currVal = currVal;
+  ip.inputType = inputType;
+  ip.inputReturnButton = inputReturnButton;
+  ip.hasForward = hasForward;
+  ip.hasBackward = hasBackward;
+  ip.maxLength = maxLength;
+  ip.cursorPos = cursorPos;
+
+  [ip show];
 }
 JSAG_MEMBER_END
 
@@ -81,20 +81,21 @@ JSAG_OBJECT_END
 
 + (void) addToRuntime:(js_core *)js {
     m_js = js;
-    
+
     JSAG_OBJECT_ATTACH(js.cx, js.native, input);
 }
 
 + (void) onDestroyRuntime {
-	if (m_js) {
-		InputPrompt *instance = [InputPrompt get];
-        
-		if (instance) {
-			[instance.inputPromptAlertView dismissWithClickedButtonIndex:0 animated:FALSE];
-		}
-	}
-    
-	m_js = nil;
+  if (m_js) {
+//    InputPrompt *instance = [InputPrompt get];
+
+    // TODO should this dismiss the view controller on the root controller?
+//    if (instance) {
+//      [instance.inputPromptAlertController dismissWithClickedButtonIndex:0 animated:FALSE];
+//    }
+  }
+
+  m_js = nil;
 }
 
 @end
