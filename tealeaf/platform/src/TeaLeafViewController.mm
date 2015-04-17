c10793e37aaa46fdddd1ec6fae5bde481579a68c
Martin Hunt <martin@gameclosure.com>
2015-04-17 16:16:49 -0700

    include image mime type for base64 data

--- tealeaf/platform/src/TeaLeafViewController.mm
+++ tealeaf/platform/src/TeaLeafViewController.mm
@@ -571,7 +571,7 @@ CEXPORT void device_hide_splash() {
 	}
 
     NSData *data = UIImagePNGRepresentation(image);
-    NSString *b64Image = encodeBase64(data);
+    NSString *b64Image = [NSString stringWithFormat:@"image/png;base64,%@", encodeBase64(data)];
     [[PluginManager get] dispatchJSEvent:@{ @"name" : @"PhotoLoaded", @"url": self.photoURL, @"data": b64Image }];
     
     [self dismissViewControllerAnimated:YES completion:NULL];
